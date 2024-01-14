128d923765542d8b557901e762f8c1c5c83aac47
antonbashlovka <anton.bashlovka@gmail.com>
2024-01-14 12:39:07 +0500

    added javadoc to person and student

--- Lesson_5/MVCStudentAppHomework/src/Model/Domain/Person.java
+++ Lesson_5/MVCStudentAppHomework/src/Model/Domain/Person.java
@@ -4,20 +4,45 @@ public class Person {
     private String name;
     private int age;
 
+    /**
+     * Конструктор класса Person 
+     * @param name
+     * @param age
+     */
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
+    /**
+     * Геттер. Возвращает имя персонажа
+     * @return String
+     */
     public String getName() {
         return name;
     }
+
+
+    /**
+     * Сеттер. Задает имя персонажа
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
+
+    /**
+     * Геттер. Возвращает возраст персонажа
+     * @return int
+     */
     public int getAge() {
         return age;
     }
+
+    /**
+     * Сеттер. Задает возраст персонажа
+     * @param age
+     */
     public void setAge(int age) {
         this.age = age;
     }
--- Lesson_5/MVCStudentAppHomework/src/Model/Domain/Student.java
+++ Lesson_5/MVCStudentAppHomework/src/Model/Domain/Student.java
@@ -6,23 +6,44 @@ public class Student {
     private String name;
     private int age;
 
+    /**
+     * Конструктор класса Student 
+     * @param name
+     * @param age
+     */
     public Student(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
+    /**
+     * Геттер. Возвращает имя студента
+     * @return String 
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Сеттер. Задает имя студента
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Геттер. Возвращает возраст студента
+     * @return int
+     */
     public int getAge() {
         return age;
     }
 
+    /**
+     * Сеттер. Задает возраст студента
+     * @param age
+     */
     public void setAge(int age) {
         this.age = age;
     }
