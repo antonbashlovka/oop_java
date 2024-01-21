903bd822ec1f842757a5d933dcb8e0964062167a
antonbashlovka <anton.bashlovka@gmail.com>
2024-01-21 18:36:43 +0500

    Start task2 - ocp

--- Lesson_6/OOP1_Homework/src/App.java
+++ Lesson_6/OOP1_Homework/src/App.java
@@ -4,6 +4,7 @@
  */
 
 import task1_SRP.*;
+import task2_OCP.*;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -12,14 +13,16 @@ public class App {
     public static void main(String[] args) throws Exception {
 
 
-        
+        // task 1 - srp
         SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
         Date date = formatter.parse("22-01-2000");
         Employee worker = new Employee("Serj", date, 20000);
         System.out.println(worker.getEmpInfo());
 
-
-        
+        //task 2 - ocp
+        Vehicle vehicle = new Vehicle(80, "Bus");
+        SpeedCalculation speed = new SpeedCalculation();
+        System.out.println(speed.calculateAllowedSpeed(vehicle));
         
     }
 }
