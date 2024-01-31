69da6ff8084f7cb0433feb324345d0586e92160c
antonbashlovka <anton.bashlovka@gmail.com>
2024-01-31 13:25:48 +0500

    done fabric method

--- Lesson_7/Fabric/src/App.java
+++ Lesson_7/Fabric/src/App.java
@@ -1,5 +1,34 @@
+import Fabric.ItemGenerator;
+import Fabric.GoldGenerator;
+import Fabric.GemGenerator;
+
+import java.util.ArrayList;
+
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        ItemGenerator f2 = new GoldGenerator();
+        f2.openReward();
+
+        
+        ItemGenerator f3 = new GemGenerator();
+        f3.openReward();
+
+        List<ItemGenerator> generatorList = new ArrayList<>();
+        generatorList.add(new GoldGenerator());
+        generatorList.add(new GemGenerator());
+
+        Random rnd = ThreadLocalRandom.current();
+        
+        for (int i=0; i<10; i++ ){
+            int index = rnd.nextInt(2);
+            generatorList.get(index).openReward();
+        }
+
+
     }
 }
